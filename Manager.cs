ec8665764f56f35ee4408962e518f5eb76f00de7
congxi <[bofenghui@gmail.com]>
2019-01-05 17:29:30 +0900

    0

--- Manager.cs
+++ Manager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manger<T> : MonoBehaviour where T : MonoBehaviour
+public class Manager<T> : MonoBehaviour where T : MonoBehaviour
 {
     static T instance;
 
